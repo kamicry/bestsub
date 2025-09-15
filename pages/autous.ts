4fd5f95cabf6bd0865784b517e320f3554360dff
ChenBaicheng <51500612+armjukl@users.noreply.github.com>
2025-09-15 13:13:59 +0800

    Update autous.ts
    自动订阅

--- pages/autous.ts
+++ pages/autous.ts
@@ -1,8 +1,9 @@
 // pages/autous.ts
 import { GetServerSideProps, GetServerSidePropsContext } from 'next';
-import { NextApiResponse } from 'next';
 
-export const getServerSideProps: GetServerSideProps = async ({ res }: { res: NextApiResponse }) => {
+export const getServerSideProps: GetServerSideProps = async (context: GetServerSidePropsContext) => {
+  const { res } = context;
+
   // 设置重定向头部
   res.writeHead(302, { Location: 'https://sub.miku0.dpdns.org/api/subscribe?host=run.idc0.eu.org&uuid=1ad24c65-3f62-4234-a1e8-290d4c2de009&path=1ad24c65&type=ws' });
   res.end();
